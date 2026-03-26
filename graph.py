31f7fa495354407661c22139b23daa183a88671c
VivekRegulus <128822236+VivekRegulus@users.noreply.github.com>
2026-03-27 02:38:06 +0530

    Modify graph methods for edge cost and travel time
    Updated add_edge method to include speed parameter and added rush_hour_factor and travel_cost methods for calculating travel time based on traffic conditions.

--- graph.py
+++ graph.py
@@ -10,14 +10,33 @@ class Graph:
         if node not in self.edges:
             self.edges[node] = []
 
-    def add_edge(self, u, v, cost):
-        self.edges[u].append((v, cost))
-        self.edges[v].append((u, cost))
+    def add_edge(self, u, v, distance, speed=20):
+        self.edges[u].append((v,distance,speed))
+        self.edges[v].append((u,distance,speed))
 
     def get_neighbors(self, node):
         return self.edges.get(node, [])
 
-    def heuristic(self, node, goal):
-        x1, y1 = self.coords[node]
-        x2, y2 = self.coords[goal]
-        return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+    def rush_hour_factor(self,time):
+        hour = time%24
+        minute = (time % 1)*60
+        if hour >=18 or hour < 8 or hour == 13:
+            return 1.0  # normal travel time during night hours and lunch hour
+        if minutes >=0 and minutes minutes <10:
+            return 1.2 #rush hour during students going to classes slightly late or leaving 
+        if minutes < 60 and minutes >= 50:
+            return 1.5 #peak rush hours of class going students
+        return 1.0
+
+    #Cost Function
+    def travel_cost(self,u,v,current_time):
+        for edge in self.edges[u]:
+            neighbour = edge[0] 
+            distance = edge[1] 
+            speed = edge[2]
+            if neighbour == v:
+                normal_time = distance/speed
+                travel_time = normal_time *rush_hour_factor(current_time)
+                return travel_time
+        
+   
